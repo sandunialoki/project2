1e648b5a9694c4a7df8bb3b0c9c10ba7a1d4a4ca
Sandunialoki <sanduni.alk98@gmail.com>
2021-02-21 20:12:47 +0530

    second commit

--- src/section3/Main.java
+++ src/section3/Main.java
@@ -3,6 +3,7 @@ package section3;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("This is my first project at SLIIT");
+		System.out.println("This is my first project at home");
 	}
 
 }
